9736a2702c31cfc94f8288f803a2ed8bcd7e3a3e
Rizky Dhani <rizkydhani15@gmail.com>
2024-05-10 15:39:00 +0700

    added Bab Akreditasi CRUD

---
+++ app/Livewire/Accreditation/Bab/BabEdit.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Accreditation\Bab;
+
+use Livewire\Component;
+
+class BabEdit extends Component
+{
+    public function render()
+    {
+        return view('livewire.accreditation.bab.bab-edit');
+    }
+}
