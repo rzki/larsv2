064b055a879120823fe944d95c9cf589a3f7b305
Rizky Dhani <rizkydhani15@gmail.com>
2024-05-10 16:27:00 +0700

    added detail Akreditasi page

--- resources/views/livewire/accreditation/accreditation-show.blade.php
+++ resources/views/livewire/accreditation/accreditation-show.blade.php
@@ -18,4 +18,29 @@
         <!-- end row -->
     </div>
     <!-- ========== title-wrapper end ========== -->
+
+    <div class="row mb-4">
+        <div class="card-styles">
+            <div class="card-style-3 d-grid mb-30">
+                <button class="btn d-flex justify-content-between fs-5 text-start" type="button" data-bs-toggle="collapse" data-bs-target="#daftarSurveyorDropdown" aria-expanded="false" aria-controls="daftarSurveyorDropdown" aria-expanded="false">Daftar Surveyor <i class="mdi mdi-chevron-down text-end"></i></button>
+                <div class="collapse" id="daftarSurveyorDropdown">
+                    <div class="card card-body border-0 mt-3">
+                        <div class="table-wrapper">
+                            <table class="table striped-table text-black table-responsive">
+                                <thead>
+                                    <th scope="col">No</th>
+                                    <th scope="col">Nama Bab</th>
+                                    <th scope="col">SARS 22 Manual</th>
+                                    <th scope="col">SARS 22 Floating</th>
+                                    <th scope="col">SARS 22 Average</th>
+                                    <th scope="col">TDD</th>
+                                    <th scope="col"></th>
+                                </thead>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
