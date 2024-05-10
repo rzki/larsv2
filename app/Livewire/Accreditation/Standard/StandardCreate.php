d5c64d99f111ff106a9ecdddbf631395bb77e9f8
Rizky Dhani <rizkydhani15@gmail.com>
2024-05-10 15:39:49 +0700

    added Standar Akreditasi CRUD

---
+++ app/Livewire/Accreditation/Standard/StandardCreate.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Accreditation\Standard;
+
+use Livewire\Component;
+
+class StandardCreate extends Component
+{
+    public function render()
+    {
+        return view('livewire.accreditation.standard.standard-create');
+    }
+}
