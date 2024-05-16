04f8c5dcd71f5749df0522146e2b2da1e084edac
Rizky Dhani <rizkydhani15@gmail.com>
2024-05-16 09:57:48 +0700

    added register accreditation page

---
+++ app/Livewire/Public/AccreditationRegister.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Public;
+
+use Livewire\Component;
+
+class AccreditationRegister extends Component
+{
+    public function render()
+    {
+        return view('livewire.public.accreditation-register');
+    }
+}
